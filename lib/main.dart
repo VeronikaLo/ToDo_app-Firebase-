31c67956ef70beba701ae30b0d7fa4c015016772
Veronika L <verokipri@yahoo.com>
2022-10-26 22:37:48 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/injection.dart' as di;
 import 'package:todo_app/presentation/home/signup/signup_page.dart';
@@ -6,6 +7,7 @@ import 'app_theme.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
   runApp(const MyApp());
 }
